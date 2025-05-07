17a26940d097dafe01e5140ff685007b0556c505
Stu Frankish <stuart.frankish@duendesoftware.com>
2025-05-07 12:46:08 +0100

    Updated to MudBlazor style.

--- WasmHosted/Client/Program.cs
+++ WasmHosted/Client/Program.cs
@@ -2,10 +2,16 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using WasmHosted.Client;
 
+using MudBlazor.Services;
+
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+// Add MudBlazor services to the DI container
+builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
